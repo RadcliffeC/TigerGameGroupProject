36d220ee1885e4e759e5045e77e413d2077b64b0
Chandler Radcliffe <ck.radcliffe@gmail.com>
2025-04-22 08:06:51 -0500

    Initial Commit

---
+++ Tiger Game Group Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Tiger Game Group Project
+//
+//  Created by Chandler Radcliffe on 4/22/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

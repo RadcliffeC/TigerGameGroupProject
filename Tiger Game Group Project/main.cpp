e686bf797058c4e671b2712a6cd07235c43f6426
RadcliffeC <ck.radcliffe@gmail.com>
2025-04-23 17:53:12 -0500

    main for project

--- Tiger Game Group Project/main.cpp
+++ Tiger Game Group Project/main.cpp
@@ -1,14 +1,78 @@
-//
-//  main.cpp
-//  Tiger Game Group Project
-//
-//  Created by Chandler Radcliffe on 4/22/25.
-//
+//============================================================================
+// Name        : Bear.cpp
+// Author      : Dr. Booth
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Tiger Game
+//============================================================================
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include "SDL_Plotter.h"
+#include "Line.h"
+#include "BearGame.h"
+
+int main() {
+
+    SDL_Plotter g(ROWS, COLUMNS);
+
+    BearGame game(g);
+    int x,y;
+
+
+    while(!g.getQuit()){
+
+
+        if(g.kbhit()){
+            switch(g.getKey()){
+
+            }
+        }
+
+        if(g.getMouseDown(x,y)){
+            game.checkForClickOnToken(Point(x,y));
+        }
+
+        if(g.getMouseUp(x,y)){
+            if(game.isOver() && game.getPause()){
+                game.setGameOver(false);
+                g.clear();
+                game = BearGame(g);
+            }
+            else{
+                game.updateTokenLocation(Point(x,y), g);
+                if(!game.isTigersTurn() && game.tigerWins()){
+                    game.displayFile(Point(150,450),"Tiger.bmp.txt",g);
+                    game.setGameOver(true);
+                    game.setPause(true);
+                }
+
+                else if(game.isTigersTurn() && game.manWins()){
+                    game.displayFile(Point(210,450),"manwins.bmp.txt",g);
+                    game.setGameOver(true);
+                    game.setPause(true);
+                }
+            }
+
+
+        }
+
+        if(g.getMouseMotion(x,y)){
+
+            game.checkAttachedToken(Point(x,y), g);
+        }
+
+        if(game.isTigersTurn()){
+            //game.randomMove(g); //Move Tiger
+        }
+        else{
+            //game.randomMove(g); //Move Man
+        }
+
+
+        game.draw(g);
+        g.update();
+    }
     return 0;
 }
